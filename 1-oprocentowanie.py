8006f135f59154262d09cbb51be19d7b682e55e3
Maciej Dems <maciej.dems@p.lodz.pl>
2019-10-08 11:51:45 +0200

    Szablon zadania

---
+++ 1-oprocentowanie.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+"""
+# Oprocentowanie
+
+Niech p będzie rocznym oprocentowaniem konta bankowego. Po n latach początkowa suma A wzrośnie do:
+
+A (1 + p / 100)<sup>n</sup>
+
+Napisz funkcję `interest` (przyjmującą argumenty A, n i p), która zwraca kwotę na koncie po kilku latach,
+a następnie użyj jej do wyliczenia ile pieniędzy będzie na koncie po trzech latach, jeśli początkowy kapitał wynosił
+1000 zł, a oprocentowanie 5%.
+"""
---
+++ 5-krotkie-slowa.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+"""
+# Krótkie słowa
+
+Napisz funkcję `get_short_words`, która przyjmie listę wyrazów. Funkcja powinna zwrócić listę słów krótszych od n
+znaków, gdzie n_ jest maksymalną ilością liter w wyrazie. Domyślnie _n powinno mieć wartość 5.
+
+##### Przykład
+```python
+wyrazy = "Litwo ojczyzno moja ty jesteś jak zdrowie".split(' ')
+
+print(get_short_words(wyrazy))
+print(get_short_words(wyrazy, 3))
+```
+
+Wynik:
+```
+['moja', 'ty', 'jak']
+['ty']
+```
+
+##### Zadanie dodatkowe
+Sprawdź w dokumentacji działanie metody `split` działającej na łańcuch tekstowych i użytej w przykładzie.
+"""
