6610de787ef38175f6fc47575c891320efb05d79
Maciej Dems <maciej.dems@p.lodz.pl>
2019-10-08 11:51:45 +0200

    Szablon zadania

---
+++ 1-oprocentowanie.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+"""
+# Oprocentowanie
+
+Niech p będzie rocznym oprocentowaniem konta bankowego. Po n latach początkowa suma A wzrośnie do:
+
+               n
+A (1 + p / 100)
+
+Napisz funkcję `interest` (przyjmującą argumenty A, n i p), która zwraca kwotę na koncie po kilku latach,
+a następnie użyj jej do wyliczenia ile pieniędzy będzie na koncie po trzech latach, jeśli początkowy kapitał wynosił
+1000 zł, a oprocentowanie 5%.
+"""
---
+++ 2-temperatura.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+"""
+# Temperatura
+
+Wzór przeliczający temperaturę w stopniach Fahrenheita na stopnie Celsiusza jest następujący:
+
+C = 5/9 (F − 32)
+
+Napisz funkcję ``C(F)``, która wylicza temperaturę według tego wzoru. Następnie napisz program, który drukuję tabelę
+ze stopniami Fahrenheita: 0, 10, 20, . . . , 100 w pierwszej kolumnie oraz odpowiadającą im temperaturę w stopniach
+Celsiusza w drugiej kolumnie.
+"""
